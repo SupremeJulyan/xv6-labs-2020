eb15ef655a26caa0beb640ee8805845871187178
SupremeJulyan <2369505159@qq.com>
2025-10-24 09:41:48 +0800

    julyan 打印函数调用栈

--- kernel/riscv.h
+++ kernel/riscv.h
@@ -318,6 +318,14 @@ sfence_vma()
   // the zero, zero means flush all TLB entries.
   asm volatile("sfence.vma zero, zero");
 }
+//获取当前执行函数的帧指针
+static inline uint64
+r_fp()
+{
+  uint64 x;
+  asm volatile("mv %0, s0" : "=r" (x) );
+  return x;
+}
 
 
 #define PGSIZE 4096 // bytes per page
