b0cef59ee42deb69d6b2044e01e294374eb04946
SupremeJulyan <2369505159@qq.com>
2025-10-25 22:23:19 +0800

    julyan 写时拷贝

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,6 +67,19 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
+  }else if(r_scause() == 13 || r_scause()  == 15) {
+    uint64 fault_va = r_stval();  // 获取出错的虚拟地址
+      if(fault_va < p->sz){
+        void *pa = cowalloc(p->pagetable, PGROUNDDOWN(fault_va));
+        if(pa == 0){
+          p->killed = 1;
+        }
+      }else{
+        p->killed = 1;
+      } 
+    
+    
+
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
